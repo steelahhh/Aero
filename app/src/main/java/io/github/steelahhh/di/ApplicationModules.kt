47e33092870baf73d61ecc63ca29cb4a0e83c9f8
Alexander <mrkennedyua@gmail.com>
2019-05-24 16:45:10 +0500

    Application Module

---
+++ app/src/main/java/io/github/steelahhh/di/ApplicationModules.kt
@@ -0,0 +1,25 @@
+package io.github.steelahhh.di
+
+import io.github.steelahhh.core.Navigator
+import io.github.steelahhh.core.SchedulersProvider
+import io.github.steelahhh.core.SchedulersProviderImpl
+import io.github.steelahhh.data.local.di.localModule
+import io.github.steelahhh.data.remote.di.remoteModule
+import io.github.steelahhh.feature.search.di.searchModule
+import org.koin.dsl.module
+import org.koin.experimental.builder.single
+
+val appModule = module {
+    single<Navigator>()
+    single<SchedulersProvider> {
+        SchedulersProviderImpl()
+    }
+}
+
+val applicationModules = listOf(
+    appModule,
+    remoteModule,
+    localModule
+) + listOf(
+    searchModule
+)
