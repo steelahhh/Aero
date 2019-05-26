0757e3a30297cbd05f587e8919705554c8839b19
Alexander <mrkennedyua@gmail.com>
2019-05-26 21:17:49 +0500

    Show search controller from the main activity

--- app/src/main/java/io/github/steelahhh/feature/MainActivity.kt
+++ app/src/main/java/io/github/steelahhh/feature/MainActivity.kt
@@ -3,13 +3,35 @@ package io.github.steelahhh.feature
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
 import io.github.steelahhh.R
+import io.github.steelahhh.core.Navigator
+import io.github.steelahhh.feature.character.search.SearchController
+import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val navigator: Navigator by inject()
+
+    private lateinit var router: Router
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+
+        navigator.bind(router)
+
+        navigator.setRoot(SearchController())
+    }
+
+    override fun onBackPressed() {
+        if (!router.handleBack()) {
+            super.onBackPressed()
+        }
     }
 }
